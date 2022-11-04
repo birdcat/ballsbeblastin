a88cba351b336dd90eac8cee43b383e4a181139a
birdcat <ameliagrace628@gmail.com>
2022-11-03 22:17:37 -0400

    commiting stuff

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+cannon_dictionary = {
+    # example: cannonname : [mainimage, boughtimage, notboughtimage, mass, coincount, boughtornot]
+    "c1": ["images/cannons/c1main", "images/cannons/c1bought", "images/cannons/c1notbought", 10, 0, True],
+    "c1": ["images/cannons/c2main", "images/cannons/c2bought", "images/cannons/c1notbought", 10, 0, False],
+    "c1": ["images/cannons/c3main", "images/cannons/c3bought", "images/cannons/c1notbought", 10, 0, False],
+    "c1": ["images/cannons/c4main", "images/cannons/c4bought", "images/cannons/c1notbought", 10, 0, False],
+    "c1": ["images/cannons/c5main", "images/cannons/c1bought", "images/cannons/c1notbought", 10, 0, False],
+    "c1": ["images/cannons/c6main", "images/cannons/c1bought", "images/cannons/c1notbought", 10, 0, False]
+}
+
+ball_dictionary = {
+    #example = ballname : [mainimage, boughtimage, notboughtimage, mass, velocity, coincountm, boughtornot]
+}
