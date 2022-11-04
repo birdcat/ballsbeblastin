09742321290c345c1fcd65fcb6e3fdf6af32535d
xcao2005 <xcao2005@gmail.com>
2022-11-04 11:37:59 -0400

    made properties inside menu class

--- classes.py
+++ classes.py
@@ -1,20 +1,9 @@
 import pygame, sys
 
 #global variables
-screen_width = 1000
-screen_height = 500
-window = pygame.display.set_mode((screen_width,screen_height))
-windowclock = pygame.time.Clock()
 current_screen = 1
 current_cannon="m1"
-current_ball="ball1"
-
-width_border=screen_width/100
-height_border=screen_height/50
-menu_label_width=screen_width/2
-menu_label_height=screen_height/5
-menu_stats_y=height_border+menu_label_height+10
-menu_stats_height=screen_height-menu_stats_y-height_border
+current_ball="b1"
 
 pygame.font.init()
 stats_font = pygame.font.SysFont('Comic Sans MS', 50)
@@ -24,17 +13,18 @@ button_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 
 class Button:
-
-    def __init__(self, text, pos, size):
+    def __init__(self, text, pos, size, window):
         self.x, self.y = pos
         self.font = button_font
         self.text = self.font.render(text, False, 'white')
         self.surface = pygame.Surface(size)
         self.surface.blit(self.text, (0, 0))
         self.rect = pygame.Rect(self.x, self.y, size[0], size[1])
+        self.window=window
+
 
     def draw(self):
-        window.blit(self.surface, (self.x, self.y))
+        self.window.blit(self.surface, (self.x, self.y))
 
     def click(self, event):
         x, y = pygame.mouse.get_pos()
@@ -45,31 +35,43 @@ class Button:
 
 class Menu(object):
     def __init__(self):
+        self.screen_width = 1000
+        self.screen_height = 500
+        self.window = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.windowclock = pygame.time.Clock()
+        self.width_border = self.screen_width / 100
+        self.height_border = self.screen_height / 50
+        self.menu_label_width = self.screen_width / 2
+        self.menu_label_height = self.screen_height / 5
+        self.menu_stats_y = self.height_border + self.menu_label_height + 10
+        self.menu_stats_height = self.screen_height - self.menu_stats_y - self.height_border
         self.Main()
 
     def Main(self):
-        window.fill((255, 255, 255))
-        menu_label = pygame.Rect(width_border, height_border, menu_label_width, menu_label_height)
-        pygame.draw.rect(window, 'black', menu_label, 5)
-        menu_stats = pygame.Rect(width_border, menu_stats_y, menu_label_width, menu_stats_height)
-        pygame.draw.rect(window, 'black', menu_stats, 5)
+        self.window.fill((255, 255, 255))
+        menu_label = pygame.Rect(self.width_border, self.height_border, self.menu_label_width, self.menu_label_height)
+        pygame.draw.rect(self.window, 'black', menu_label, 5)
+        menu_stats = pygame.Rect(self.width_border, self.menu_stats_y, self.menu_label_width, self.menu_stats_height)
+        pygame.draw.rect(self.window, 'black', menu_stats, 5)
 
         cannon_mass_text = stats_font.render('Cannon Mass:', False, (0, 0, 0))
-        window.blit(cannon_mass_text, (2*width_border, menu_stats_y+height_border))
+        self.window.blit(cannon_mass_text, (2*self.width_border, self.menu_stats_y+self.height_border))
         ball_mass_text = stats_font.render('Ball Mass:', False, (0, 0, 0))
-        window.blit(ball_mass_text, (2*width_border, menu_stats_y+50+height_border))
+        self.window.blit(ball_mass_text, (2*self.width_border, self.menu_stats_y+50+self.height_border))
         ball_velocity_text = stats_font.render('Ball Velocity:', False, (0, 0, 0))
-        window.blit(ball_velocity_text, (2 * width_border, menu_stats_y + 100 + height_border))
+        self.window.blit(ball_velocity_text, (2 * self.width_border, self.menu_stats_y + 100 + self.height_border))
 
         button_store = Button(
             "Store",
-            (2 * width_border, menu_stats_y + 170 + height_border),
+            (2 * self.width_border, self.menu_stats_y + 170 + self.height_border),
             (100, 50),
+            self.window
             )
         button_game = Button(
             "Play",
-            (2 * width_border, menu_stats_y + 220 + height_border),
+            (2 * self.width_border, self.menu_stats_y + 220 + self.height_border),
             (100, 50),
+            self.window
             )
 
 
@@ -87,7 +89,7 @@ class Menu(object):
             button_store.draw()
             button_game.draw()
             pygame.display.update()
-            windowclock.tick(60)
+            self.windowclock.tick(60)
 
 
 
