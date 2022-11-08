f3d83b2d0418df70ad9fcbf1dd47eebafd1f795b
birdcat <ameliagrace628@gmail.com>
2022-11-08 08:56:43 -0500

    fixed store glitch

--- classes.py
+++ classes.py
@@ -121,20 +121,14 @@ class Store(object):
         yph = 50
         countph = 0
         for key in cannon_dict:
-            self.cannons.add(
-                self.Storebutton(xph, yph + countph, key, "c", cannon_dict[key]["m"], "", cannon_dict[key]["cost"],
-                                 cannon_dict[key]["boughtimg"], cannon_dict[key]["notboughtimg"],
-                                 cannon_dict[key]["bought"]))
+            self.cannons.add(self.Storebutton(xph, yph + countph, key, "c", cannon_dict[key]["m"], "", cannon_dict[key]["cost"], cannon_dict[key]["boughtimg"], cannon_dict[key]["notboughtimg"], cannon_dict[key]["bought"]))
             countph += 200
             if countph >= 600:
                 xph = 220
                 countph = 0
         xph = 570
         for key in ball_dict:
-            self.balls.add(self.Storebutton(xph, yph + countph, key, "b", ball_dict[key]["m"], ball_dict[key]["v"],
-                                            ball_dict[key]["cost"],
-                                            ball_dict[key]["boughtimg"], ball_dict[key]["notboughtimg"],
-                                            ball_dict[key]["bought"]))
+            self.balls.add(self.Storebutton(xph, yph + countph, key, "b", ball_dict[key]["m"], ball_dict[key]["v"], ball_dict[key]["cost"], ball_dict[key]["boughtimg"], ball_dict[key]["notboughtimg"], ball_dict[key]["bought"]))
             countph += 200
             if countph >= 600:
                 xph = 770
